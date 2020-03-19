b101e972de9a8737042cfd3f91236c559d2e2754
Warui Ngugi <waruingugientp@gmail.com>
2020-03-20 00:41:08 +0300

    Changed database setting

--- outwithcorona/settings.py
+++ outwithcorona/settings.py
@@ -128,8 +128,7 @@ STATIC_URL = '/static/'
 # Whitenoise causing errors, now using django-static
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
 # Django Security Settings
 SECURE_BROWSER_XSS_FILTER = True
